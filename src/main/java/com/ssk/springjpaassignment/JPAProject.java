808f2f6386160f7d2ab2829787d626f293365f51
SivaRayal <kuruva.siva8055@gmail.com>
2023-10-16 06:41:27 +0530

    Initial commit

--- src/main/java/com/ssk/springjpaassignment/JPAProject.java
+++ src/main/java/com/ssk/springjpaassignment/JPAProject.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringjpaassignmentApplication {
+public class JPAProject {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringjpaassignmentApplication.class, args);
+		SpringApplication.run(JPAProject.class, args);
 	}
 
 }
